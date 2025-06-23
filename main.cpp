d9c800c2288120871b652b8b5524e70443a2f9ea
BogDan Vatra <bogdan@kde.org>
2025-06-23 15:24:34 +0300

    Fixed led blink

--- main.cpp
+++ main.cpp
@@ -16,11 +16,12 @@ void setup_led_pio() {
     sm_config_set_clkdiv(&c, 1);
 
     sm_config_set_sideset_pin_base(&c, PICO_DEFAULT_LED_PIN);
+
+
     pio_gpio_init(pio, PICO_DEFAULT_LED_PIN);
     pio_sm_set_consecutive_pindirs(pio, sm, PICO_DEFAULT_LED_PIN, 1, true);
 
     // Auto pull/push
-    sm_config_set_in_shift(&c, true, true, 32);
     sm_config_set_out_shift(&c, true, true, 32);
 
     pio_sm_init(pio, sm, offset, &c);
@@ -39,15 +40,13 @@ int main()
     puts("\033[2J\033[HHello there !\n");
 
     setup_led_pio();
-
     uint32_t data = 0;
     while(true) {
         sleep_ms(500); // at this point the led should be ON for 500ms
         pio_sm_put_blocking(pio, sm, data++);
 
         sleep_ms(500); // at this point the led should be OFF for 500ms
-        uint32_t dat = pio_sm_get_blocking(pio, sm);
-        printf("%lx\n", dat);
+        pio_sm_put_blocking(pio, sm, data++);
     }
     return 0;
 }
